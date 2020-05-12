a7236e7a85bf29b1d372334196202c679edddcff
Latha <Latha@DESKTOP-GP0TL91>
2020-05-12 16:36:27 +0100

    update

--- src/main/java/io/javabrains/CourseApi3Application.java
+++ src/main/java/io/javabrains/CourseApi3Application.java
@@ -2,6 +2,7 @@ package io.javabrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CourseApi3Application {
@@ -9,5 +10,11 @@ public class CourseApi3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApi3Application.class, args);
 	}
+	
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CourseApi3Application.class); 
+	} 
+
 
 }
