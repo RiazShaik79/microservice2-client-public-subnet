d1b8c5711dd50040b59de8114dc564aa8cebf50f
Latha <Latha@DESKTOP-GP0TL91>
2020-05-12 16:00:58 +0100

    Initial Commit

---
+++ src/main/java/io/javabrains/Topic.java
@@ -0,0 +1,44 @@
+package io.javabrains;
+
+
+
+public class Topic {
+	
+	
+	private String Id;
+	private String name;
+	private String desp;
+	
+
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesp() {
+		return desp;
+	}
+	public void setDesp(String desp) {
+		this.desp = desp;
+	}
+	
+	public Topic() {
+		
+	}
+	
+	public Topic(String id, String name, String desp) {
+		super();
+		Id = id;
+		this.name = name;
+		this.desp = desp;
+	}
+	
+
+}
